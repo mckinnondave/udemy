fae47c0bb36138a9cc0e62db87e43a061643b0f3
Dave <dave.w.mckinnon@gmail.com>
2022-08-06 14:39:01 -0700

    add Udemy answer for reversing

--- data-structures-and-algorithms/8_reverseString.js
+++ data-structures-and-algorithms/8_reverseString.js
@@ -1,16 +1,32 @@
 const reverse = (str) => {
-  return str.split('').reverse().join('')
-}
+  return str.split("").reverse().join("");
+};
 
 console.log("Reverse:", reverse("Hi my name is Dave"));
 
 const longReverse = (str) => {
-  let array = str.split('')
-  let reversedString = ""
+  let array = str.split("");
+  let reversedString = "";
   for (let i = array.length - 1; i >= 0; i--) {
-     reversedString += array[i]   
+    reversedString += array[i];
   }
-  return reversedString
-}
+  return reversedString;
+};
 
 console.log("Long Reverse:", longReverse("Hi my name is Dave"));
+
+// Udemy solution
+function udemyReverse(str) {
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "hmm that is not good";
+  }
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+  return backwards.join("");
+}
+
+console.log("Udemy Answer:", udemyReverse("Hi my name is Dave"));
