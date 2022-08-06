c986eb24972a3fad00bc907a7cca56d8327361a7
Dave <dave.w.mckinnon@gmail.com>
2022-08-06 15:21:18 -0700

    add udemy answer and refactor my own solution

--- data-structures-and-algorithms/9_mergeArrays.js
+++ data-structures-and-algorithms/9_mergeArrays.js
@@ -1,7 +1,40 @@
-const mergeSortedArrays = (arr1, arr2) => {
-  return arr1.concat(arr2).sort((a, b) => {
-    return a - b;
-  })
-}
+// const mergeSortedArrays = (arr1, arr2) => {
+//   return arr1.concat(arr2).sort((a, b) => {
+//     return a - b;
+//   })
+// }
+
+const mergeSortedArrays = (arr1, arr2) => arr1.concat(arr2).sort((a, b) => a - b);
 
 console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+
+// Udemy Answer
+function mergeArrays(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item) {
+    if(!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item)
+      array1Item = array1[i]
+      i++
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+console.log("UDEMY:", mergeArrays([0, 3, 4, 31], [4, 6, 30]));
