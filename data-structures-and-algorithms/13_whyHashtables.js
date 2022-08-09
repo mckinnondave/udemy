21a198e0e872044cc6a125b4d107250ebd0a60a9
Dave <dave.w.mckinnon@gmail.com>
2022-08-09 13:35:21 -0700

    add short notes about why we use hash tables

---
+++ data-structures-and-algorithms/13_whyHashtables.js
@@ -0,0 +1,3 @@
+// Arrays have O(n) for search while Hash Tables are O(1), which is true as well for inserting data. Arrays may have to loop to add something new while Hash Tables simply adds the key/value pair (unless there is collision)
+
+// Issue with hashtable => no concept of order unlike arrays that have 'keys' of numbers
