87b0043343938a4ee841395d9b8b0fc40be6aaa0
Dave <dave.w.mckinnon@gmail.com>
2022-08-09 11:37:21 -0700

    create set and get for hashTables

--- data-structures-and-algorithms/12_hashTables2.js
+++ data-structures-and-algorithms/12_hashTables2.js
@@ -10,10 +10,33 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] =[];
+    } 
+    this.data[address].push([key, value]);
+    return this.data
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i  = 0; i  < currentBucket.length; i ++) {
+        if(currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }        
+      }
+    } // If no collisions, this is O(1). O(n) with collisions.
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
-myHashTable.get('grapes');
+myHashTable.set('apples', 54);
+console.log("GET RETURN", myHashTable.get('grapes'));
 
-console.log(myHashTable);
+console.log(myHashTable.data);
