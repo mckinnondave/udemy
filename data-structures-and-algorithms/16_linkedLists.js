b46f78f059d0705a90aaad56b5a129294a54c185
Dave <dave.w.mckinnon@gmail.com>
2022-08-13 12:25:36 -0700

    add short about section on linked lists

---
+++ data-structures-and-algorithms/16_linkedLists.js
@@ -0,0 +1,12 @@
+// ABOUT
+
+// Has a head node connected to other nodes that end with a tail and terminated with null
+
+ const basket = ['apples', 'grapes', 'pears'];
+
+//  linked list: apples => grapes => pears
+
+// apples
+// 8947 => grapes
+//          8742 => pears
+//                  372 => null
