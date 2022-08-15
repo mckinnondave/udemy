7231f6432f5a45f99c15bad6e0af419acf003cdf
Dave <dave.w.mckinnon@gmail.com>
2022-08-15 11:01:54 -0700

    add about for doubly linked lists

---
+++ data-structures-and-algorithms/18_doublyLInkedLists.js
@@ -0,0 +1,11 @@
+// Doubly Linked Lists ABOUT
+
+// Has a pointer to the next node, but that next node also has a pointer on the second block back to the previous node.
+
+// Still has a head and tail, and terminates with Null
+
+// Can start from the end and go backwards down your list unlike Singly LInked Lists that can only go forwards
+
+// Searching can be more efficient
+
+// May have to hold more memory because of the extra block
