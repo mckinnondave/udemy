5f58fd26ca7fae5784543960f3919f32e6760004
Dave <dave.w.mckinnon@gmail.com>
2022-08-15 14:55:38 -0700

    revise js files to md as needed

---
+++ data-structures-and-algorithms/15_hashTableReview.md
@@ -0,0 +1,11 @@
+## Hashtables
+
+### PROS
+- Fast Lookups (Good collision resolution needed)
+- Fast inserts
+- Flexible Keys
+
+
+### CONS
+- Unordered
+- Slow key iteration  
--- data-structures-and-algorithms/19_singlyVsDoublyLinkedLists.js
+++
@@ -1,7 +0,0 @@
-// Single vs Double?
-
-// Single: simple implementation, less memory, faster (less operations). Cannot be traversed backwards.Use if you have less memory and need fast insertion/deletion
-
-// Double: Traversed front or back. Complex to implement. More memory. Good when memory is not a factor and great for searching.
-
-// Mostly going to see singly in interview questions.
