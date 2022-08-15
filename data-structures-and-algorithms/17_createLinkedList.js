85ba80ce94f677bede8eaee180b66ab9252ec003
Dave <dave.w.mckinnon@gmail.com>
2022-08-15 10:50:51 -0700

    add insert method to linked list

--- data-structures-and-algorithms/17_createLinkedList.js
+++ data-structures-and-algorithms/17_createLinkedList.js
@@ -42,11 +42,49 @@ class LinkedList {
     this.length++;
     return this;
   }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    // check params
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null
+    };
+    const leader = this.traverseToIndex(index - 1)
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
+myLinkedList.insert(200, 99)
+myLinkedList.insert(2, 99)
+console.log("Printed List", myLinkedList.printList());
 
 console.log(myLinkedList);
