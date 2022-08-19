2c14872cfec4dc1de4fc99b6e40962a4fc028a85
Dave <dave.w.mckinnon@gmail.com>
2022-08-19 16:12:58 -0700

    create lookup method

--- data-structures-and-algorithms/binarySTExercise.js
+++ data-structures-and-algorithms/binarySTExercise.js
@@ -35,7 +35,22 @@ class BinarySearchTree {
       }
     }
   }
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while(currentNode) {
+      if(value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -46,6 +61,7 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+console.log("LOOKUP", tree.lookup(170));
 console.log(JSON.stringify(traverse(tree.root)));
 
 function traverse(node) {
