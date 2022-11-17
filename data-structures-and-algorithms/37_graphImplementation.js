c7ae150fbeb4762ac7d4b05181af36012c39b2ba
Dave <dave.w.mckinnon@gmail.com>
2022-11-17 08:26:08 -0800

    finish creating graph structure

--- data-structures-and-algorithms/37_graphImplementation.js
+++ data-structures-and-algorithms/37_graphImplementation.js
@@ -4,9 +4,12 @@ class Graph {
     this.adjacentList = {};
   }
   addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
   }
   addEdge(node1, node2) {
-
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
   }
   showConnections() {
   }
