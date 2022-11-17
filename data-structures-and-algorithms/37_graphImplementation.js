03a648f6fbe093b0ff56feb21bec6ac47692b587
Dave <dave.w.mckinnon@gmail.com>
2022-11-17 08:16:29 -0800

    add initial structure for Graph

---
+++ data-structures-and-algorithms/37_graphImplementation.js
@@ -0,0 +1,23 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+  addVertex(node) {
+  }
+  addEdge(node1, node2) {
+
+  }
+  showConnections() {
+  }
+}
+
+const myGraph = new Graph ();
+myGraph.addVertex('0')
+myGraph.addVertex('1')
+myGraph.addVertex('2')
+myGraph.addVertex('3')
+myGraph.addVertex('4')
+myGraph.addVertex('5')
+myGraph.addVertex('6')
+myGraph.addVertex('7')
